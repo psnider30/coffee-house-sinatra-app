d8b6c02364726b60a69e55c6285ae28866e16fd4
psnider30 <pbsnider@gmail.com>
2017-08-29 16:19:08 -0700

    Created coffee_houses.erb to see all coffee houses and added slug methods

--- app/controllers/coffee_houses_controller.rb
+++ app/controllers/coffee_houses_controller.rb
@@ -1,7 +1,12 @@
 class CoffeeHousesController < ApplicationController
 
   get '/coffee_houses' do
-    erb :'coffee_houses/coffee_houses'
+    if logged_in?
+      @coffee_houses = CoffeeHouse.all
+      erb :'coffee_houses/coffee_houses'
+    else
+      redirect '/login'
+    end
   end
 
 end
--- app/models/coffee_house.rb
+++ app/models/coffee_house.rb
@@ -3,4 +3,12 @@ class CoffeeHouse < ActiveRecord::Base
   has_many :coffee_house_users
   has_many :users, through: :coffee_house_users
 
+  def slug
+    name.downcase.gsub(' ','-')
+  end
+
+  def self.find_by_slug(slug)
+    CoffeeHouse.all.find { |coffee_house| coffee_house.slug == slug }
+  end
+
 end
