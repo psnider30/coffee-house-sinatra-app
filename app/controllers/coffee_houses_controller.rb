c76ca6c670760c35bd2a71efe8d6c35c113962ca
psnider30 <pbsnider@gmail.com>
2017-08-29 17:19:30 -0700

    complete new coffe_house routes and form

--- app/controllers/coffee_houses_controller.rb
+++ app/controllers/coffee_houses_controller.rb
@@ -9,4 +9,30 @@ class CoffeeHousesController < ApplicationController
     end
   end
 
+  get '/coffee_houses/new' do
+    if logged_in?
+      erb :'coffee_houses/create_coffee_house'
+    else
+      redirect '/login'
+    end
+  end
+
+  post '/coffee_houses/new' do
+    if params[:name] == '' || params[:location] == '' || params[:review] == ''
+      redirect '/coffee_houses/new'
+    else
+      @coffee_house = current_user.coffee_houses.create(name: params[:name], location: params[:location], review: params[:review])
+      redirect "/coffee_houses/#{@coffee_house.slug}"
+    end
+  end
+
+  get '/coffee_houses/:slug' do
+    if logged_in?
+      @coffee_house = CoffeeHouse.find_by_slug(params[:slug])
+      erb :'coffee_houses/show_coffee_house'
+    else
+      redirect '/login'
+    end
+  end
+
 end
